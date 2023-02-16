7524451f4801c0926543fd4981099c9a27216d37
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2023-02-15 22:41:10 -0600

    created sql database

---
+++ SQL/01_Db_Create.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [trainer] (
+  [id] int PRIMARY KEY,
+  [FireBaseUserId] nvarchar(255) NOT NULL,
+  [name] nvarchar(255) NOT NULL,
+  [gender] nvarchar(255) NOT NULL
+)
+GO
+
+CREATE TABLE [ownedPokemon] (
+  [trainerId] int,
+  [id] int PRIMARY KEY,
+  [spriteUrl] nvarchar(255),
+  [name] nvarchar(255),
+  [type] varchar,
+  [weight] int,
+  [height] int
+)
+GO
+
+ALTER TABLE [ownedPokemon] ADD FOREIGN KEY ([trainerId]) REFERENCES [trainer] ([id])
+GO
