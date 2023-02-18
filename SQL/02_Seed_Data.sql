38440e30662f6138d1212ae680ccb2c7609308c5
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2023-02-18 11:46:12 -0600

    created seed script

--- SQL/02_Seed_Data.sql
+++ SQL/02_Seed_Data.sql
@@ -1,1 +1,22 @@
-﻿
+﻿USE [UltiDex]
+GO
+
+SET IDENTITY_INSERT trainer ON
+INSERT INTO trainer
+	(id, FireBaseUserId, [name], gender)
+VALUES
+	(1, '', 'Red', 'Boy')
+SET IDENTITY_INSERT trainer OFF
+
+SET IDENTITY_INSERT ownedPokemon ON
+INSERT INTO ownedPokemon
+	(trainedId, id, spriteUrl, [name], [type], [weight], [height])
+VALUES
+	(1, 
+	4, 
+	'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+	'Charmander',
+	'fire',
+	85,
+	6)
+SET IDENTITY_INSERT ownedPokemon OFF
